f43f6bf5f583560eb709ac7bf62d40123a472d25
Adrian Brady <ajbrady999@protonmail.com>
2023-10-13 11:16:12 -0500

    clean up routing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -23,21 +23,12 @@ func main() {
         logger: logger,
     }
 
-    mux := http.NewServeMux()
-
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-
     // register file server as handler for all URL paths which start with
     // "static". Strip /static from matching paths
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-    mux.HandleFunc("/", app.home)
-    mux.HandleFunc("/snippet/view", app.snippetView)
-    mux.HandleFunc("/snippet/create", app.snippetCreate)
 
     logger.Info("starting server", slog.Any("addr", *addr))
 
-    err := http.ListenAndServe(*addr, mux)
+    err := http.ListenAndServe(*addr, app.routes())
 
     logger.Error(err.Error())
     os.Exit(1)
