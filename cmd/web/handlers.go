adb3a0272be04a530279f0ea549eb958434055ed
Adrian Brady <ajbrady999@protonmail.com>
2023-10-15 19:10:52 -0500

    implement view method

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -41,7 +41,16 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+    snippet, err := app.snippets.Get(id)
+    if err != nil {
+        if errors.Is(err, models.ErrNoRecord) {
+            app.notFound(w)
+        } else {
+            app.serverError(w, r, err)
+        }
+    }
+
+    fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
