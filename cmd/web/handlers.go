56dee7b790b26cb51e782bee70da457df5f3c1a5
Adrian Brady <ajbrady999@protonmail.com>
2023-10-15 23:14:37 -0500

    modify snippetView() to render view.tmpl

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,7 @@ package main
 import (
     "fmt"
     "errors"
-    //"html/template"
+    "html/template"
     "net/http"
     "strconv"
 
@@ -58,9 +58,26 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
         } else {
             app.serverError(w, r, err)
         }
+        return
     }
 
-    fmt.Fprintf(w, "%+v", snippet)
+    files := []string{
+        "./ui/html/base.tmpl",
+        "./ui/partials/nav.tmpl",
+        "./ui/html/pages/view.tmpl",
+    }
+
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        app.serverError(w, r, err)
+        return
+    }
+
+    err = ts.ExecuteTemplate(w, "base", snippet)
+    if err != nil {
+        app.serverError(w, r, err)
+    }
+
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
