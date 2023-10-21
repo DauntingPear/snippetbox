efee2cf42a79f12346a61bceccd89261e5bb3d42
Adrian Brady <ajbrady999@protonmail.com>
2023-10-20 19:59:29 -0500

    use new router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,13 +15,14 @@ func (app *application) routes() http.Handler {
     })
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+    router.Handler(http.MethodGet ,"/static/", http.StripPrefix("/static", fileServer))
 
-    mux.HandleFunc("/", app.home)
-    mux.HandleFunc("/snippet/view", app.snippetView)
-    mux.HandleFunc("/snippet/create", app.snippetCreate)
+    router.HandlerFunc(http.MethodGet, "/", app.home)
+    router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+    router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+    router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreatePost)
 
     standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-    return standard.Then(mux)
+    return standard.Then(router)
 }
